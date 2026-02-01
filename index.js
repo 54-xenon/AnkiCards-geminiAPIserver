1b53e0deb5984f92df7cf1321db8d726322650c4
54-xenon <congas.rabbi_0b@icloud.com>
2026-02-01 17:02:37 +0900

    envとexpressの追加

---
+++ index.js
@@ -0,0 +1,23 @@
+import express  from "express";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from 'dotenv';
+
+dotenv.config({path: '../.env'});
+const apiKey = process.env.GEMINI_API_KEY;
+
+// expressの初期化
+const app = express();
+// 開くポートを指定 
+const port = 8080;
+
+// GoogleGenAIの初期化 
+const ai = new GoogleGenAI({
+    // API keyをここに記述
+    apiKey
+ });
+
+
+// 指定のポートで開く
+app.listen(port, () => {
+    console.log("server running http://localhost:${port} ")
+});
